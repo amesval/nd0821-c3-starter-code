8804429ea49bc8df9d627ef20963bc0bb51fbd1c
amesval <amesval.91@gmail.com>
2023-12-10 03:08:36 -0600

    add test and clean code

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,7 +1,11 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+# from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from starter.ml.data import process_data
+import pickle
 
 
-# Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
@@ -17,8 +21,52 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
+    # model = LogisticRegression(max_iter=1000, random_state=123)
+    model = RandomForestClassifier(n_estimators=600, 
+                                   max_depth=15, 
+                                   random_state=123456789, 
+                                   max_features=None)
+    model.fit(X_train, y_train)
+    return model
 
-    pass
+def save_model_attributes(model_attributes, filename):
+    """
+    Save model attributes to pickle file.
+
+    Inputs
+    ------
+    model_attributes : dict
+        Dictionary which contains different attributes
+        to make predictions with the model:
+            a. model: Trained machine learning model.
+            b. encoder: OneHotEncoder for categorical variables.
+            c. lb: LabelBinarizer for binary variables.
+            d. cat_features: Categorical features use for preprocessing.
+    """
+    with open(filename, 'wb') as f:
+        pickle.dump(model_attributes, f)
+
+
+def load_model_attributes(filename):
+    """
+    Load model attributes.
+
+    Inputs
+    ------
+    filename: filename for model attributes.
+    Returns
+    -------
+    model_attributes : dict
+        Dictionary which contains different attributes
+        to make predictions with the model:
+            a. model: Trained machine learning model.
+            b. encoder: OneHotEncoder for categorical variables.
+            c. lb: LabelBinarizer for binary variables.
+            d. cat_features: Categorical features use for preprocessing.
+    """
+    with open(filename, 'rb') as f:
+        model_attributes = pickle.load(f)
+    return model_attributes
 
 
 def compute_model_metrics(y, preds):
@@ -43,18 +91,75 @@ def compute_model_metrics(y, preds):
     return precision, recall, fbeta
 
 
-def inference(model, X):
+def inference(model_attributes, X):
     """ Run model inferences and return the predictions.
 
     Inputs
     ------
-    model : ???
-        Trained machine learning model.
-    X : np.array
+    model_attributes : dict
+        Dictionary which contains different attributes
+        to make predictions with the model:
+            a. model: Trained machine learning model.
+            b. encoder: OneHotEncoder for categorical variables.
+            c. lb: LabelBinarizer for binary variables.
+            d. cat_features: Categorical features use for preprocessing.
+    X : pd.DataFrame
         Data used for prediction.
     Returns
     -------
     preds : np.array
         Predictions from the model.
     """
-    pass
+    model = model_attributes['model']
+    encoder = model_attributes['encoder']
+    lb = model_attributes['lb']
+    cat_features = model_attributes['cat_features']
+
+    X_preprocessed, _, encoder, lb = process_data(
+    X, categorical_features=cat_features, label=None, training=False,
+    encoder=encoder, lb=lb)
+    preds = model.predict(X_preprocessed)
+    return preds
+
+
+def performance_for_feature_slice(dataset, feature, model_attributes):
+    """ Calculate performance over a particular feature.
+    The metrics are calculated for each feature value.
+    The results are written into "./model/slice_output.txt".
+
+    Inputs
+    ------
+    dataset: pd.DataFrame
+        Dataset which includes the training features.
+    feature: str
+        Dataset column name.
+    model_attributes : dict
+        Dictionary which contains different attributes
+        to make predictions with the model:
+            a. model: Trained machine learning model.
+            b. encoder: OneHotEncoder for categorical variables.
+            c. lb: LabelBinarizer for binary variables.
+            d. cat_features: Categorical features use for preprocessing.
+    """
+    model = model_attributes['model']
+    encoder = model_attributes['encoder']
+    lb = model_attributes['lb']
+    cat_features = model_attributes['cat_features']
+    if feature not in cat_features:
+        raise ValueError("Invalid value. The feature should be a categorical feature.")
+    with open("./model/slice_output.txt", 'w') as f:
+        for value in dataset[feature].unique():
+            feat_filter = dataset[feature] == value
+            X, y, encoder, lb = process_data(
+                dataset[feat_filter], 
+                categorical_features=cat_features, 
+                label="salary", 
+                training=False,
+                encoder=encoder, lb=lb
+            )
+            y_preds = model.predict(X)
+            precision, recall, f1 = compute_model_metrics(y, y_preds)
+            f.write(f"feature: {feature}    value: {value}    ")
+            f.write(f"precision: {np.round(precision,4)}    ")
+            f.write(f"recall: {np.round(recall, 4)}    ")
+            f.write(f"f1: {np.round(f1, 4)}\n")
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,14 +1,43 @@
 # Script to train machine learning model.
-
+import argparse
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from starter.ml.data import process_data
+from starter.ml.model import (train_model, 
+                      save_model_attributes, 
+                      load_model_attributes,
+                      compute_model_metrics,
+                      performance_for_feature_slice)
 
-# Add the necessary imports for the starter code.
-from ml.data import process_data
+parser = argparse.ArgumentParser(description='Training and evaluation of a ML model')
+parser.add_argument('--csv_dataset', 
+                    type=str, 
+                    help='CSV dataset', 
+                    default='./data/clean_census.csv')
+parser.add_argument('--model_name', 
+                    type=str, 
+                    help='Name of the trained model', 
+                    default='./model/model.pkl')
+parser.add_argument('--test_size',
+                    type=float,
+                    help='Proportion of test set',
+                    default=0.2
+                    )
+parser.add_argument('--feature_to_slice',
+                    type=str,
+                    help='Evaluate metrics for a fix feature',
+                    default='workclass')
+parser.add_argument('--random_seed',
+                    type=int,
+                    help='Random seed for the experiment',
+                    default=123456789
+                    )
+args = parser.parse_args()
 
-# Add code to load in the data.
-
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+np.random.seed(args.random_seed)
+data = pd.read_csv(args.csv_dataset)
+training_set, test_set = train_test_split(data, test_size=args.test_size)
 
 cat_features = [
     "workclass",
@@ -20,10 +49,61 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+    training_set, categorical_features=cat_features, label="salary", training=True,
+    encoder=None, lb=None
 )
 
-# Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test_set, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+model_attributes = {
+    'model': model,
+    'encoder': encoder,
+    'lb': lb,
+    'cat_features': cat_features
+    }
+
+save_model_attributes(model_attributes, args.model_name)
+
+# Compute classification metrics in the training set.
+y_preds = model.predict(X_train)
+training_precision, training_recall, training_f1= compute_model_metrics(y_train, y_preds)
+print("Training set metrics:")
+print(f"precision: {np.round(training_precision,4)}")
+print(f"recall: {np.round(training_recall, 4)}")
+print(f"f1: {np.round(training_f1, 4)}\n")
+
+
+
+# Compute classification metrics in the test set.
+model_attributes = load_model_attributes(args.model_name)
+model = model_attributes['model']
+y_preds = model.predict(X_test)
+test_precision, test_recall, test_f1 = compute_model_metrics(y_test, y_preds)
+print("Test set metrics:")
+print(f"precision: {np.round(test_precision,4)}")
+print(f"recall: {np.round(test_recall, 4)}")
+print(f"f1: {np.round(test_f1, 4)}\n")
+
+report_name = args.model_name.replace(".pkl","").split("/")[-1]
+with open(f'./model/metrics_report_{report_name}.txt', 'w') as f:
+    f.write("Training set metrics:\n")
+    f.write(f"precision: {np.round(training_precision,4)}    ")
+    f.write(f"recall: {np.round(training_recall, 4)}    ")
+    f.write(f"f1: {np.round(training_f1, 4)}\n")
+    f.write("Test set metrics:\n")
+    f.write(f"precision: {np.round(test_precision,4)}    ")
+    f.write(f"recall: {np.round(test_recall, 4)}    ")
+    f.write(f"f1: {np.round(test_f1, 4)}\n")
+
+performance_for_feature_slice(test_set, args.feature_to_slice, model_attributes)
+
+
+
+# TODO: Start writing the API
