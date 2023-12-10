f4dec256bbaf020ce61708bf5321cf274d3bd591
amesval <amesval.91@gmail.com>
2023-12-10 14:29:29 -0600

    script to call post method

---
+++ starter/post_to_api.py
@@ -0,0 +1,27 @@
+import argparse
+import json
+import requests
+
+parser = argparse.ArgumentParser(description='Inference with API')
+
+default_url = 'https://salary-prediction-api-s2x0.onrender.com/inference'
+parser.add_argument('--endpoint_url',
+                    type=str,
+                    help='Endpoint to make prediction',
+                    default=default_url)
+parser.add_argument('--input_filename',
+                    type=str,
+                    help="Name of the filename to make inference",
+                    default='./api_body_example_neg.json'
+                    )
+args = parser.parse_args()
+
+# Read an example (Body)
+with open(args.input_filename, 'r') as f:
+    data = json.load(f)
+data = json.dumps(data)
+
+# Make inference in the API
+response = requests.post(args.endpoint_url, data=data)
+response = response.json()
+print(response)
